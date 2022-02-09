2e9d60c80337551923eb7160f55f30e797e4ef65
SK M&Service 이두승 <leedoseu@gmail.com>
2022-02-09 14:46:24 +0900

    게시판 CRUD추가완료

--- src/main/java/com/duvis/practice/springboot/service/posts/PostsService.java
+++ src/main/java/com/duvis/practice/springboot/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.duvis.practice.springboot.service.posts;
 
 import com.duvis.practice.springboot.domain.posts.Posts;
 import com.duvis.practice.springboot.domain.posts.PostsRepository;
+import com.duvis.practice.springboot.web.dto.PostsListResponseDto;
 import com.duvis.practice.springboot.web.dto.PostsResponseDto;
 import com.duvis.practice.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,11 +31,26 @@ public class PostsService {
         return id;
 
     }
-
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        postsRepository.delete(posts);
+    }
+
 }
--- src/main/java/com/duvis/practice/springboot/web/PostsApiController.java
+++ src/main/java/com/duvis/practice/springboot/web/PostsApiController.java
@@ -4,6 +4,7 @@ import com.duvis.practice.springboot.service.posts.PostsService;
 import com.duvis.practice.springboot.web.dto.PostsResponseDto;
 import com.duvis.practice.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,5 +32,11 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
