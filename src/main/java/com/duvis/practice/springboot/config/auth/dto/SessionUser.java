31291157e44137dc25dc72193b6cd51aa397ccb5
SK M&Service 이두승 <leedoseu@gmail.com>
2022-02-10 14:53:06 +0900

    OAuth 로그인 기능 추가

---
+++ src/main/java/com/duvis/practice/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.duvis.practice.springboot.config.auth.dto;
+
+import com.duvis.practice.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
